41d878e8ca7d68deb3a0e7b23c84af3d9f05a608
Danil Agliullin <danil.agliullin@rt.ru>
2023-07-07 16:00:39 +0300

    fitst commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BaseTest {
+    @Test
+    public void test1() {
+        System.out.println("HELLO WORLD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        Assert.assertTrue(false);
+    }
+}
